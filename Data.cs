cd67729da7862db80cc5ac91837ca168f5d128f1
coyot7 <grzestomiak@gmail.com>
2016-04-21 22:34:48 +0200

    Rysowanie silnie spójnych składowych

--- Data.cs
+++ Data.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Aplikacja
 {
     public class Data
@@ -30,7 +31,7 @@ namespace Aplikacja
             this.row = row;
             this.col = col;
             
-            this.tab = new int[row, col];    
+            this.tab = new int[row, col];
         }
 
         public void Write(int row, int col, int data)
@@ -54,15 +55,56 @@ namespace Aplikacja
                 }
             }
 
+            //for (int i = 0; i < row; i++)
+            //{
+            //    for (int j = 0; j < col; j++)
+            //    {
+            //        {
+            //            listaTablic[i].Add(tab[i,j]); //macierz w listach 1:1
+            //        }
+            //    }
+            //}
             for (int i = 0; i < row; i++)
             {
                 for (int j = 0; j < col; j++)
                 {
+                    if (tab[i,j] == 1)
                     {
-                        listaTablic[i].Add(tab[j,i]);
+                        listaTablic[i].Add(j); //macierz w listach 1:1
                     }
                 }
             }
+            return listaTablic;
+        }
+
+        public List<int>[] ToListTransponowany(List<int>[] lista)
+        {
+            List<int>[] listaTablic = new List<int>[col];
+
+            for (int i = 0; i < row; i++)
+            {
+                {
+                    listaTablic[i] = new List<int>();
+                }
+            }
+
+            //for (int i = 0; i < row; i++)
+            //{
+            //    for (int j = 0; j < col; j++)
+            //    {
+            //        {
+            //            listaTablic[i].Add(j,i); //macierz po translacji
+            //        }
+            //    }
+            //}
+            for (int i = 0; i < row; i++)
+            {
+                foreach (int element in lista[i])
+                {
+                    listaTablic[element].Add(i);
+                }
+            }
+
             return listaTablic;
         }
     }
--- Form1.cs
+++ Form1.cs
@@ -14,14 +14,60 @@ namespace Aplikacja
 {
     public partial class Form1 : Form
     {
+        public const int max_n = 10000;
+        public List<int>[] gS; //wyjście graf SSS
+        public int[] sss = new int[max_n]; //wyjscie: numer SSS wierzchołka i
+        public int ns; //wyjscie ilosc ns
+        public int[] sssF = new int[max_n];
+        public int ttt;
+        public bool[] sssvis = new bool[max_n];
+        public int n;
+        public string[] grafSilny = new string[20];
+
+        public int licznikNextButton = 0;
+        public List<int>[] lista;
+        public List<int>[] listaTransponowana;
+
+        //public int[] visited; //dfs 
+        //public int[] NR; //numer post order w DFS
+        //public int nr = 1; //do numeracji
+        //public int[] visitedSSS; // do Silnie spójnej składowej
+        //public List<int>[] silnaWierzcholki; //wierzchołki należące do silnie spójnej składowej
+        //public int licznikSilnych = 0; //ilosc silnych
+
         public Form1()
         {
             InitializeComponent();
-            
+
             viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
             viewer.ToolBarIsVisible = false;
             viewer.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Controls.Add(viewer);
+            //######################################################
+            gS = new List<int>[max_n];
+            for (int i = 0; i < max_n; i++)
+            {
+                {
+                    gS[i] = new List<int>();
+                }
+            }
+
+
+
+
+
+            //visited = new int[10000];
+            //NR = new int[10000];
+            //silnaWierzcholki = new List<int>[10000];
+            //for (int i = 0; i < 10000; i++)
+            //{
+            //    {
+            //        silnaWierzcholki[i] = new List<int>();
+            //    }
+            //}
+            //visitedSSS = new int[10000];
+
+
             //graph = new Microsoft.Msagl.Drawing.Graph("graph");
             //create a graph object 
             //  Data macierz;
@@ -40,7 +86,7 @@ namespace Aplikacja
             graph.FindNode("B").Attr.FillColor = Microsoft.Msagl.Drawing.Color.Green;
             graph.FindNode("D").Attr.FillColor = Microsoft.Msagl.Drawing.Color.Green;
             graph.AddNode("E, F");
-            graph.AddEdge("E, F", "A");
+            graph.AddEdge("E,F", "A");
 
             Microsoft.Msagl.Drawing.Node c = graph.FindNode("C");
             c.Attr.FillColor = Microsoft.Msagl.Drawing.Color.Green;
@@ -126,16 +172,26 @@ namespace Aplikacja
 
                 graph = new Microsoft.Msagl.Drawing.Graph("graph");
 
+                //for (int i = 0; i < macierz.RowGet; i++)
+                //{
+                //    for (int j = 0; j < macierz.ColGet; j++)
+                //    {
+                //        if (macierz.Read(i, j) == 1)
+                //        {
+                //            graph.AddEdge((i + 1).ToString(), (j + 1).ToString());
+                //        }
+                //    }
+                //} //prawidłowa macierz zaczytująca plik do grafu.
+                lista = macierz.ToList();
+
                 for (int i = 0; i < macierz.RowGet; i++)
                 {
-                    for (int j = 0; j < macierz.ColGet; j++)
+                    foreach (int element in lista[i])
                     {
-                        if (macierz.Read(i, j) == 1)
-                        {
-                            graph.AddEdge((i + 1).ToString(), (j + 1).ToString());
-                        }
+                        graph.AddEdge((i + 1).ToString(), (element + 1).ToString());
                     }
                 }
+
                 viewer.Graph = graph;
 
             }
@@ -171,40 +227,156 @@ namespace Aplikacja
 
         private void Next_Click(object sender, EventArgs e)
         {
-            
-            this.graph.Edges.Clear();
-            
-            viewer.Graph = graph;
-            viewer.Refresh();
-            
-            //for (int i = 0; i < macierz.RowGet; i++)
-            //{
-            //    for (int j = 0; j < macierz.ColGet; j++)
-            //    {
-            //        if (macierz.Read(j, i) == 1)
-            //        {
-            //            graph.AddEdge((i + 1).ToString(), (j + 1).ToString());
-            //        }
-            //    }
-            //} działa prawidłowo dla macierzy.
+            //lista = macierz.ToList();
+            listaTransponowana = macierz.ToListTransponowany(lista);
 
-            List<int>[] lista = macierz.ToList();
-            for (int i = 0; i < macierz.RowGet; i++)
+            if (licznikNextButton == 0)
             {
-                int licznik = 1;
-                foreach (int element in lista[i])
-                {
-                    if (element == 1)
-                    {
-                        graph.AddEdge((i + 1).ToString(), licznik.ToString());
+                this.graph.Edges.Clear();
 
+                viewer.Graph = graph;
+                viewer.Refresh();
+
+                //for (int i = 0; i < macierz.RowGet; i++)
+                //{
+                //    for (int j = 0; j < macierz.ColGet; j++)
+                //    {
+                //        if (macierz.Read(j, i) == 1)
+                //        {
+                //            graph.AddEdge((i + 1).ToString(), (j + 1).ToString());
+                //        }
+                //    }
+                //} działa prawidłowo dla macierzy.
+
+                // lista = macierz.ToList();
+
+                for (int i = 0; i < macierz.RowGet; i++)
+                {
+                    foreach (int element in listaTransponowana[i])
+                    {
+                        graph.AddEdge((i + 1).ToString(), (element + 1).ToString());
                     }
-                    licznik++;
                 }
+
+
+                //for (int i = 0; i < macierz.RowGet; i++)
+                //{
+                //    int licznik = 1;
+                //    foreach (int element in listaTransponowana[i])
+                //    {
+                //        if (element == 1)
+                //        {
+                //            graph.AddEdge((i + 1).ToString(), licznik.ToString());
+
+                //        }
+                //        licznik++;
+                //    }
+                //}
+
+                viewer.Graph = graph;
+                viewer.Refresh();
+                licznikNextButton++;
+            }
+            else if (licznikNextButton == 1)
+            {
+                n = macierz.RowGet;
+
+                for (int i = 0; i < n; ++i)
+                {
+                    sssvis[i] = false;
+                    gS[i].Clear();
+                }
+                ttt = 0;
+                for (int i = 0; i < n; ++i)
+                {
+                    if (!sssvis[i])
+                    {
+                        sss_dfs(i);
+                    }
+                }
+                for (int i = 0; i < n; ++i)
+                {
+                    sssvis[i] = false;
+                }
+                ns = 0;
+                for (int i = ttt - 1; i >= 0; --i)
+                {
+                    if (!sssvis[sssF[i]])
+                    {
+                        sss_dfs2(sssF[i]);
+                        ++ns;
+                    }
+                }
+                //MessageBox.Show("Liczba sss: " + ns);
+                //for (int i = 0; i < ns; ++i)
+                //{
+                //    MessageBox.Show("Skladowa d: " + (i + 1));
+                //    for (int v = 0; v < n; ++v)
+                //        if (sss[v] == i)
+                //        {
+                //            MessageBox.Show("Wierzchołek " + (v + 1));
+                //        }
+                //}
+                //#######################################
+               
+                this.graph.Edges.Clear();
+                this.graph.NodeMap.Clear();
+                viewer.Graph = graph;
+                viewer.Refresh();
+
+                for (int i = 0; i < ns; ++i)
+                {
+                    //string wyrazenie = null;
+
+                    for (int v = 0; v < n; ++v)
+                        if (sss[v] == i)
+                        {
+                            grafSilny[i] += (v + 1).ToString() + ",";
+                        }
+
+                   // graph.AddEdge((i + 1).ToString(), wyrazenie);
+                }
+                for (int i = 0; i < 19; i++)
+                {
+                    if (grafSilny[i + 1] != null)
+                        graph.AddEdge(grafSilny[i].Remove(grafSilny[i].Length - 1), grafSilny[i + 1].Remove(grafSilny[i + 1].Length - 1));
+                }
+
+                viewer.Graph = graph;
+                viewer.Refresh();
+                licznikNextButton++;
             }
 
-            viewer.Graph = graph;
-            viewer.Refresh();
+        }
+
+        public void sss_dfs(int v)
+        {
+            sssvis[v] = true;
+            
+            //for (int i = 0; i < macierz.RowGet; i++)
+            {
+                foreach(int element in lista[v])
+                {
+                    if (!sssvis[element])
+                        sss_dfs(element);
+
+                }
+            }
+            sssF[ttt++] = v;
+        }
+
+        public void sss_dfs2(int v)
+        {
+            sss[v] = ns;
+            sssvis[v] = true;
+            //for (int i = 0; i < macierz.RowGet; i++)
+            {
+                foreach (int element in listaTransponowana[v])
+                {
+                    if (!sssvis[element])
+                        sss_dfs2(element);
+                }
+            }
         }
     }
 }
